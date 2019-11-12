c754f6c5bf31a56997368116d465000638e3dd0f
Marvin Wong <47411170+mdswong@users.noreply.github.com>
2019-11-12 14:15:35 +0800

    Update getAvailableLetters.py

--- getAvailableLetters.py
+++ getAvailableLetters.py
@@ -1,3 +1,12 @@
+"""
+Create a function getAvailableLetters that takes in one parameters - a list of letters,
+lettersGuessed. This function will return a string that is comprised of lowercase
+English letters - all lowercase English letters that are not in lettersGuessed.
+"""
+
+# getAvailableLetters - a function that will show a list of the guessed letters
+# lettersGuessed - a list of letters
+
 def getAvailableLetters(lettersGuessed):
     
     alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
