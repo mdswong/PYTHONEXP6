2b35b3a851d8b140fd23ca2926c73607f8f386ca
Marvin Wong <47411170+mdswong@users.noreply.github.com>
2019-11-12 14:05:13 +0800

    Update Hangaroo.py

--- Hangaroo.py
+++ Hangaroo.py
@@ -1,3 +1,15 @@
+"""
+Create a function Hangaroo, which takes one parameter - the secretWord 
+the user is to guess. This starts up an interactive game of Hangaroo between the 
+user and the computer. Be sure you take advantage of the three helper functions, 
+isWordGuessed, getGuessedWord, and getAvailableLetters, that you've defined in 
+the previous part.
+"""
+
+# secretWord - The word to guess.
+# lettersGuessed - The letters that have been guessed so far.
+# guesses - The number of guesses left.
+
 def Hangaroo(secretWord):
     
     lettersGuessed = []
