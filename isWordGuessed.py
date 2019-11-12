9e8cc7d00725bfe05f37bcf4e148a43ff185fe17
Marvin Wong <47411170+mdswong@users.noreply.github.com>
2019-11-12 14:07:30 +0800

    Update isWordGuessed.py

--- isWordGuessed.py
+++ isWordGuessed.py
@@ -1,3 +1,13 @@
+"""
+Create a function that is named as: isWordGuessed that takes in two parameters -
+a string, secretWord, and a list of letters, lettersGuessed. This function will return a 
+boolean - True if secretWord if all the letters of secretWord as in lettersGuessed 
+and False if not.
+"""
+
+# secretWord - a string
+# lettersGuessed - a list of the letters
+
 def isWordGuessed(secretWord, lettersGuessed):
     
     strng = 0
