f6c68e9fc5514d5fc15be20f60f600f0ddfaad90
Marvin Wong <47411170+mdswong@users.noreply.github.com>
2019-11-12 14:09:58 +0800

    Update getGuessedWord.py

--- getGuessedWord.py
+++ getGuessedWord.py
@@ -1,3 +1,15 @@
+"""
+Create a function getGuessedWord that takes in two parameters - a string,
+secretWord, and a list of letters, lettersGuessed. This function will return a string
+that is comprised of letters and underscores, based on what letters in 
+lettersGuessed are in secretWord. This shouldn't be too different from 
+isWordGuessed.
+"""
+
+# secretWord - a string
+# lettersGuessed - a list of the letters
+# string - return, consists of letters and underscores
+
 def getGuessedWord(secretWord, lettersGuessed):
     
     strng = " "
